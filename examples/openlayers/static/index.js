bc52256935e2e95fa7ee6abfe9dbffae55c53230
okomojacob <jaylabs2030@gmail.com>
2022-04-22 09:08:16 +0300

    Folder restructure :hammer:

--- examples/openlayers/static/index.js
+++
@@ -1,8 +0,0 @@
-// /* global app:true */
-
-// var app = app || {};
-// /* Install express using cli
-// npm install express*/
-
-// var express = require('express');
-// app.use("/static", express.static('./static/'));
